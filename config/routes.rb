c0abf39e0c6cf996e0ed94a97b02716c577cb817
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-04 15:35:45 -0500

    add searches controller and searches routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
+  root "users#home" 
+  
   resources :inquiries 
+
+  resources :users 
   get "/signup" => "users#new"
   post "/users" => "users#create"
-
+  
+  
 
   get "/login" => "sessions#new"
   post "/sessions" => "sessions#create"
   get "/logout" => "sessions#destroy"
+
+  
+  resources :searches
+  get "/searches/:search" => "searches#index"
+  get "/searches/:id" => "searches#show"
+  
+
 end
