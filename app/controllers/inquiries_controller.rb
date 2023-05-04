949dbc4f7aeb270ad579cc5d449e7f268d036968
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-04 10:52:10 -0500

    add edit and update actions and forms in edit.html.erb

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -23,7 +23,25 @@ class InquiriesController < ApplicationController
         company: params[:inquiry][:company],
         salary: params[:inquiry][:salary],
         remote: params[:inquiry][:remote],
-        next_steps: params[:inquiry][:company],
+        next_steps: params[:inquiry][:next_steps],
+      )
+      redirect_to "/inquiries"
+    end
+
+    def edit
+      @inquiry = Inquiry.find_by(id: params[:id])
+      render :edit
+    end
+  
+    def update
+      @inquiry = Inquiry.find_by(id: params[:id])
+      @inquiry.update(
+        title: params[:inquiry][:title],
+        description: params[:inquiry][:description],
+        company: params[:inquiry][:company],
+        salary: params[:inquiry][:salary],
+        remote: params[:inquiry][:remote],
+        next_steps: params[:inquiry][:next_steps],
       )
       redirect_to "/inquiries"
     end
